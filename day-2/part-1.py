45700ad1658b6a4a6101290f272f84f23bc51a95
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-07-19 02:13:12 +0100

    Added python solutions for day 2

---
+++ day-2/part-1.py
@@ -0,0 +1,29 @@
+shapes = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+
+    "X": 0,
+    "Y": 1,
+    "Z": 2
+}
+
+
+def get_score(elf, you):
+    score = 1 + shapes[you]
+    if shapes[elf] == shapes[you]:
+        return score + 3
+    if (shapes[elf] + 1) % 3 == shapes[you]:
+        return score + 6
+    return score
+
+
+def main():
+    with open('day-2/input.txt', 'r') as f:
+        total_score = 0
+        for line in f.readlines():
+            total_score += get_score(line[0], line[2])
+        return total_score
+
+
+print(main())
---
+++ day-2/part-2.py
@@ -0,0 +1,26 @@
+shapes = {
+    "A": 0,
+    "B": 1,
+    "C": 2
+}
+
+
+def get_score(elf, you):
+    if you == 'Y':
+        return shapes[elf] + 4
+
+    if you == 'Z':
+        return (shapes[elf] + 1) % 3 + 7
+
+    return (shapes[elf] - 1) % 3 + 1
+
+
+def main():
+    with open('day-2/input.txt', 'r') as f:
+        total_score = 0
+        for line in f.readlines():
+            total_score += get_score(line[0], line[2])
+        return total_score
+
+
+print(main())
