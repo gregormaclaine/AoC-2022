cd2705c3b779b8ff4999428d194f53efcce06555
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-07-20 23:21:29 +0100

    Better rust sols for day 1

--- rust/src/bin/day-1a.rs
+++ rust/src/bin/day-1a.rs
@@ -1,31 +1,9 @@
 fn main() -> std::io::Result<()> {
-    let lines: Vec<String> = include_str!("../../../input/day-1.txt")
-        .split("\n")
-        .map(String::from)
-        .collect();
-
-    let mut running_total: i32 = 0;
-    let mut elves: Vec<i32> = vec![];
-
-    for line in lines {
-        match line.as_str() {
-            "" => {
-                elves.push(running_total);
-                running_total = 0;
-            }
-            _ => {
-                running_total += line.parse::<i32>().unwrap();
-            }
-        }
-    }
-
-    let mut max_i: usize = 0;
-    for i in 1..elves.len() {
-        if elves[i] > elves[max_i] {
-            max_i = i;
-        }
-    }
-
-    println!("{}", elves[max_i]);
+    let total: u32 = include_str!("../../../input/day-1.txt")
+        .split("\n\n")
+        .map(|x| x.split('\n').map(|x| x.parse::<u32>().unwrap()).sum())
+        .max().unwrap();
+    
+    println!("{}", total);
     Ok(())
 }
---
+++ rust/src/bin/day-1b.rs
@@ -0,0 +1,13 @@
+fn main() -> std::io::Result<()> {
+    let mut lines: Vec<u32> = include_str!("../../../input/day-1.txt")
+        .split("\n\n")
+        .map(|x| x.split('\n').map(|x| x.parse::<u32>().unwrap()).sum())
+        .collect();
+    
+    lines.sort();
+    
+    let total: u32 = (&lines[(lines.len() - 3)..lines.len()]).into_iter().sum();
+
+    println!("{}", total);
+    Ok(())
+}
