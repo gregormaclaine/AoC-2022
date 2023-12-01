05ddd50021393b2f05d910da11aec5dedd707e68
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-12-01 18:44:36 +0000

    Added asm solution for day 1a

--- asm/day-1a.asm
+++ asm/day-1a.asm
@@ -1,8 +1,30 @@
 .data
 
-input: .asciiz "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
-
+input_file: .asciiz "/Users/gregormaclaine/Projects/AoC-2022/input/day-1.txt"
+buffer: .space 12000  # 12KB
+	
 .text
+
+read_file:
+	li $v0, 13           # Load the system call code for open file
+	la $a0, input_file   # Load the address of the input file name
+	li $a1, 0            # Flag for reading
+	li $a2, 0                                       # Mode is ignored
+	syscall
+
+	move $s0, $v0                                   # Save the file descriptor returned by syscall
+
+	li $v0, 14                                      # Load the system call code for reading from file
+	move $a0, $s0                                   # Load the file descriptor to read
+	la $a1, buffer                                  # Load the address of the buffer to write into
+	li $a2, 12000                                    # Read the entire file into the buffer
+	syscall
+
+
+	li $v0, 16                                      # Load the system call for close file
+	move $a0, $s0                                   # Load the file descriptor to close
+	syscall
+        
 
 li $t0, 0  # Character index
 li $t2, 0  # Current max
@@ -10,7 +32,7 @@ li $t3, 0  # Running total
 li $t4, 0  # Current number
 
 main:
-	lb $t1, input($t0)
+	lb $t1, buffer($t0)
 	
 	beqz $t1, next_num
 	beq $t1, '\n', next_num
@@ -33,7 +55,7 @@ next_num:
 	beqz $t1, end
 	
 	addi $t0, $t0, 1
-	lb $t1, input($t0)
+	lb $t1, buffer($t0)
 	beq $t1, '\n', finished_group
 	
 	j main
@@ -53,3 +75,4 @@ end:
 
 	li $v0, 10
 	syscall
+	
---
+++ asm/test-inputs/day-1a.asm
@@ -0,0 +1,55 @@
+.data
+
+buffer: .asciiz "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000"
+
+.text
+
+li $t0, 0  # Character index
+li $t2, 0  # Current max
+li $t3, 0  # Running total
+li $t4, 0  # Current number
+
+main:
+	lb $t1, buffer($t0)
+	
+	beqz $t1, next_num
+	beq $t1, '\n', next_num
+	
+	subi $t1, $t1, 48  # Convert char to num by digit
+	
+	# Multiplies running total by 10 and adds new digit
+	add $t9, $t4, $t4  # T = 2 * cur
+	sll $t4, $t4, 3    # cur *= 8
+	add $t4, $t4, $t9  # cur += T
+	add $t4, $t4, $t1  # cur += new_digit
+	
+	addi $t0, $t0, 1
+	j main
+	
+next_num:
+	add $t3, $t3, $t4
+	li $t4, 0
+	
+	beqz $t1, end
+	
+	addi $t0, $t0, 1
+	lb $t1, buffer($t0)
+	beq $t1, '\n', finished_group
+	
+	j main
+	
+finished_group:
+	addi $t0, $t0, 1
+	move $t9, $t3
+	li $t3, 0
+	bgt $t2, $t9, main
+	move $t2, $t9
+	j main
+
+end:
+	li $v0, 1
+	move $a0, $t2
+	syscall
+
+	li $v0, 10
+	syscall
